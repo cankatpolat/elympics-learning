9bc51d78355d3de545eff311a04b584eced4361e
Cankat <cankat.17.polat@gmail.com>
2025-02-14 13:44:00 +0300

    gameInput

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializedField] private Camera mainCamera;
+    
+
+    private Vector3 GetWorldMousePosition()
+
+}
+
+public struct GatheredInput
+{
+    public Vector2 movementInput;
+    public Vector3 mousePosition;
+
+}   
+   
